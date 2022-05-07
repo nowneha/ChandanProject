7c5661440199ea45cabe2aa32e7e2b9790251715
nowneha <100633850+nowneha@users.noreply.github.com>
2022-05-07 11:15:03 -0400

    first commit

---
+++ ChandanTutorial/src/test/java/YoutubeTutorial/ChandanTutorial/TestClassMaven.java
@@ -0,0 +1,11 @@
+package YoutubeTutorial.ChandanTutorial;
+
+import org.testng.annotations.Test;
+
+public class TestClassMaven {
+@Test
+	public void TestMaven() {
+		System.out.println("hi");
+		
+	}
+}
